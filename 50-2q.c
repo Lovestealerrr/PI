2f52a567d903f0ef76952c457858de26e5d0beba
Luis Peixoto <luis.miguel.peixoto41@gmail.com>
2021-06-07 19:32:34 +0100

    teach me senpai

--- 50-2q.c
+++ 50-2q.c
@@ -9,6 +9,16 @@ int valor;
 struct lligada *prox;
 } *LInt;
 
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
 
 //1
 
@@ -64,17 +74,130 @@ LInt reverseL (LInt l){
 //5
 
 void insertOrd (LInt *l, int x){
-    LInt temp=*l;
-    while (l)
+    LInt temp=malloc(sizeof(struct lligada));
+    temp->valor=x;
+    while (*l && (*l)->valor < x)
     {
         (*l)=(*l)->prox;
-        if (x>=(*l)->valor){
-            (*l)->prox=x;
-            (*l)->prox=(*l)->prox->prox;
-            
-        }
+    }
+    nova->prox=(*l)
+    *l=nova;
+}
+
+
+//6
+
+int removeOneOrd (LInt *l, int x){
+    LInt ant = *l;
+
+    while(*l && (*l)->valor != x){
+        ant = *l;
+        l = &(*l)->prox;
     }
     
-    
-    
+    if( *l ){
+       *l = (*l)->prox;
+        ant->prox = *l;
+        return 0;
+    }
+
+    return 1;
 }
+
+//7
+
+void ordenaArray(int k[], int v[], int s[], int N, int M){
+    int l=N+M;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (v[i]<s[j])
+            {
+                k[l++]=v[i];
+            }   
+        }   
+    }
+}
+
+void merge (LInt *r, LInt a, LInt b){
+    int v[length(a)];
+    int s[length(b)];
+    for (int i = 0; a!=NULL; i++,a=a->prox)
+    {
+        v[i]=a->valor;
+    }
+    for (int i = 0; b!=NULL; i++, b=b->prox)
+    {
+        s[i]=b->valor;
+    }
+    int k[length(a)+length(b)]=ordenaArray(k,v,s,length(a),length(b));
+    (*r)->valor=k[0];
+    for (int i = 1; i<length(a)+length(b); i++)
+    {
+        (*r)->prox=k[i];
+    }
+}
+
+//2a tentativa
+
+void merge (LInt *r, LInt a, LInt b){
+    while (a && b)
+    {
+        if (a->valor< b->valor)
+        {
+            *r-valor=a->valor;
+            a=a->prox;
+            r=&((*r)->prox);
+        }else{
+            *r->valor=b->valor;
+            b=b->prox;
+            r=&((*r)->prox);
+        }
+    }
+    while(a!=NULL){
+        *r = a;
+        *r = (*r)->prox;
+    }
+    while(b!=NULL){
+        *r = b;
+        *r = (*r)->prox;
+    }
+}
+
+//8
+
+void splitQS (LInt l, int x, LInt *mx, LInt *Mx){
+    while (l)
+    {
+        if (l->valor<x)
+        {
+            *mx=l;
+            mx=&((*mx)->prox);
+        }else{
+            *Mx=l;
+            Mx=&((*Mx)->prox);
+        }
+        l=l->prox;
+    }
+    *mx=NULL;
+    *Mx=NULL;
+}
+
+//9
+
+LLig parteAmeio (LLig *l){
+    int k=length(l)/2;
+    LInt aux=malloc(sizeof(struct lligada));
+    while (l && k>0)
+    {
+        k--;
+        aux->valor=(*l)->valor;
+        aux=aux->prox;
+        l=&((*l)->prox);
+    }
+    return aux;
+}
+
+//10
+
